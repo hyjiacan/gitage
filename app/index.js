9c67adc5f62df9faa16f09bc986ddbd328f48219
hyjiacan <hyjiacan@163.com>
2020-05-22 13:38:13 +0800

    添加 全局异常处理

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 const server = require('./server')
 const config = require('./config')
+const logger = require('./misc/logger')
 
+process.on('uncaughtException', e => {
+  logger.error(e)
+})
 
 server.start(config.port, config.host)
