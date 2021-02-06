53289e3feba503a2016adafcbec566d76019dc45
hyjiacan <hyjiacan@163.com>
2021-02-06 14:59:46 +0800

    修复 checkout 代码可能失败的问题

--- app/misc/deploy.js
+++ app/misc/deploy.js
@@ -61,12 +61,17 @@ async function checkoutRepo(data) {
       // logger.info(`rmdir: ${dist}`)
       // fs.rmdirSync(dist, {recursive: true})
       // git --work-tree=${WEB_DIR} checkout --force
-      await util.runCommand(`git fetch origin ${branch} --prune ${VERBOSE}`, tempDir)
+      await util.runCommand(`git fetch origin ${branch} --prune --all ${VERBOSE}`, tempDir)
       // await util.runCommand(`git reset --hard origin/${branch}`, dist)
       const head = await util.readFileContent(path.join(tempDir, '.git', 'HEAD'))
       logger.info('Checking current branch name: ' + head)
       if (!head.startsWith(`ref: refs/heads/${branch}`)) {
         await util.runCommand(`git checkout -B ${branch} -f`, tempDir)
+      }
+      try {
+        await util.runCommand(`git merge --abort`, tempDir)
+      } catch (e) {
+        // ignore
       }
       await util.runCommand(`git pull origin ${branch} ${VERBOSE}`, tempDir)
     } else {
